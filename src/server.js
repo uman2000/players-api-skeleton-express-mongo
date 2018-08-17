cf927dca3336055ba3e045a6e1b52edc845064fb
Mike Perry <mike@alchemy.codes>
2018-08-17 14:12:26 -0400

    create and export minimal express app

--- src/server.js
+++ src/server.js
@@ -1,3 +1,30 @@
 // TODO
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('config');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
-module.exports = {};
+// Setup db
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri);
+mongoose.connection.on('error', console.error.bind(console, 'mongo connection error:'));
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/', routes);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 409);
+  res.send(err.message);
+});
+
+module.exports = app;
